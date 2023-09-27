e0836d92437a5f65e4ae9349edf72cea77d7b913
shubhamkadu7500@gmail.com <shubhamkadu7500@gmail.com>
2023-09-27 11:38:08 -0700

    we created get a user, get all user, delete a user and update a user

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -42,5 +42,62 @@ const loginUserCtrl = asyncHnadler(async(req,res)=>{
     // console.log(email,password);
 });
 
+//update a user
+const updatedUser = asyncHnadler(async(req,res)=>{
+    const {id} = req.params;
+    try{
+        const updatedUser = await User.findByIdAndUpdate(id, {
+            firstname: req?.body?.firstname,
+            lastname: req?.body?.lastname,
+            email: req?.body?.email,
+            mobile: req?.body?.mobile,
+        },{
+            new : true
+        });
+        res.json(updatedUser);
 
-module.exports = {createUser, loginUserCtrl};
+    }catch(error){
+        throw new Error(error);
+    }
+});
+
+//how to fetch or get all the user
+const getallUser = asyncHnadler(async(req,res)=>{
+    try{
+        const getUsers = await User.find();
+        res.json(getUsers);
+    }catch(error){
+        throw new Error(error);
+    }
+});
+
+//get a single user
+const getaUser = asyncHnadler(async(req,res)=>{
+    // console.log(req.params);
+    const {id} = req.params;
+    // console.log(id);
+    try{
+        const getaUser = await User.findById(id);
+        res.json({
+            getaUser,
+        });
+    }catch(error){
+        throw new Error(error);
+    }
+});
+
+//delete a single user
+const deleteaUser = asyncHnadler(async(req,res)=>{
+    // console.log(req.params);
+    const {id} = req.params;
+    try{
+        const deleteaUser = await User.findByIdAndDelete(id);
+        res.json({
+            deleteaUser
+        });
+    }catch(error){
+        throw new Error(error);
+    }
+});
+
+module.exports = {createUser, loginUserCtrl, getallUser, getaUser, deleteaUser, updatedUser};
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,19 @@
 const express = require('express');
-const { createUser, loginUserCtrl }= require('../controller/userCtrl');
+const { 
+    createUser,
+    loginUserCtrl,
+    getallUser,
+    getaUser,
+    deleteaUser,
+    updatedUser,
+
+}= require('../controller/userCtrl');
 const router = express.Router();
 
 router.post('/register', createUser);//register router
 router.post('/login', loginUserCtrl); //login router
+router.get('/all-users', getallUser); //all-users
+router.get('/:id', getaUser); // get single-user
+router.delete('/:id', deleteaUser); //delete single-user
+router.put('/:id', updatedUser); //update single-user
 module.exports = router;
